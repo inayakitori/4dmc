253658bb9ab2816abc6c7e640bf4041ab2750e56
Inaya Khurram <s3943498@student.rmit.edu.au>
2022-09-21 16:56:03 +1000

    Still broken, stepping kata/ana in quick succession breaks it

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/supportstructure/SuffocationSupport.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/supportstructure/SuffocationSupport.java
@@ -1,9 +1,15 @@
 package com.gmail.inayakitorikhurram.fdmc.supportstructure;
 
+import com.gmail.inayakitorikhurram.fdmc.FDMCConstants;
 import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.CanStep;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.block.BlockState;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShapes;
 
 public class SuffocationSupport extends SupportStructure{
 
@@ -11,24 +17,21 @@ public class SuffocationSupport extends SupportStructure{
 
     protected SuffocationSupport(ServerPlayerEntity player, BlockPos finalPos, BlockPos prevPos) {
         super.supportTypeId = 2;
+        super.stepDirection = ((CanStep) player).getStepDirection();
 
         super.world = player.getWorld();
         super.linkedPlayer = player;
-        super.activeBox = new Box(finalPos.add(0, 1, 0), finalPos.add(1, 2, 1));
-        super.finalPos = finalPos.add(0, 2, 0);
-        super.prevPos = prevPos.add(0, 2, 0);
-        super.stepDirection = ((CanStep) player).getStepDirection();
     }
 
     @Override
     protected boolean placeSupport() {
-        return ((CanStep) linkedPlayer).isStepping();
+        return ((CanStep) linkedPlayer).isStepping() && hasIntersection();
     }
 
     @Override
     protected boolean shouldRemove() {
         return (lifetime > MIN_LIFETIME &&
-                !linkedPlayer.isInsideWall()) ||
+                !hasIntersection() ) ||
                 lifetime > MAX_LIFETIME;
     }
 
@@ -39,5 +42,19 @@ public class SuffocationSupport extends SupportStructure{
         return true;
     }
 
+    protected boolean hasIntersection(){
+        Box box = linkedPlayer.getBoundingBox().offset(0, 0.01, 0);
+        return BlockPos.stream(box).anyMatch(pos -> {
+            BlockState blockState = this.world.getBlockState((BlockPos)pos);
+            return !blockState.isAir() &&
+                    blockState.shouldSuffocate(this.world, (BlockPos)pos) &&
+                    VoxelShapes.matchesAnywhere(
+                            blockState.getCollisionShape(
+                                    this.world,
+                                    (BlockPos)pos).offset(pos.getX(), pos.getY(), pos.getZ()),
+                            VoxelShapes.cuboid(box),
+                            BooleanBiFunction.AND);
+        });
+    }
 
 }
