68705ae1638dce6a790e37a837fba17ad0303d7b
Inaya Khurram <s3943498@student.rmit.edu.au>
2022-10-05 08:26:21 +1100

    Working towards implementing 4D neighbour updates

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/neighourupdaters/NeighborUpdaterMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/neighourupdaters/NeighborUpdaterMixin.java
@@ -4,10 +4,12 @@ package com.gmail.inayakitorikhurram.fdmc.mixin.neighourupdaters;
 import com.gmail.inayakitorikhurram.fdmc.FDMCConstants;
 import com.gmail.inayakitorikhurram.fdmc.FDMCMath;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.block.NeighborUpdater;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NeighborUpdater.class)
-public interface NeighborUpdaterMixin {
+public interface NeighborUpdaterMixin extends NeighbourUpdaterMixinI{
 
     @Inject(method = "updateNeighbors", at = @At("TAIL"))
     default void updateNeighborsEnd(BlockPos pos, Block sourceBlock, Direction except, CallbackInfo ci) {
