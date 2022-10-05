806d57d673ba12b9fba623b1b1f1a52ebb02b342
Inaya Khurram <s3943498@student.rmit.edu.au>
2022-10-05 13:02:08 +1100

    fixing isFullyConnected and not connected functions

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/block/AbstractBlockStateMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/block/AbstractBlockStateMixin.java
@@ -2,6 +2,8 @@ package com.gmail.inayakitorikhurram.fdmc.mixin.block;
 
 import com.gmail.inayakitorikhurram.fdmc.Direction4;
 import com.gmail.inayakitorikhurram.fdmc.FDMCMath;
+import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.AbstractBlockI;
+import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.AbstractBlockStateI;
 import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.AbstractBlock;
@@ -11,29 +13,38 @@ import net.minecraft.state.State;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.WorldAccess;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Mixin(AbstractBlock.class)
+abstract class AbstractBlockMixin implements AbstractBlockI {
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction4 direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return state;
+    }
+
+}
 @Mixin(AbstractBlock.AbstractBlockState.class)
 public abstract class AbstractBlockStateMixin
         extends State<Block, BlockState>
-        implements AbstractBlockStateMixinI{
+        implements AbstractBlockStateI {
 
     @Shadow protected abstract BlockState asBlockState();
+
+    @Shadow public abstract Block getBlock();
 
     protected AbstractBlockStateMixin(Block owner, ImmutableMap<Property<?>, Comparable<?>> entries, MapCodec<BlockState> codec) {
         super(owner, entries, codec);
     }
 
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction4 direction4, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        return state;
+    public BlockState getStateForNeighborUpdate(Direction4 direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return ((AbstractBlockI)getBlock()).getStateForNeighborUpdate(this.asBlockState(), direction, neighborState, world, pos, neighborPos);
     }
 
     @Inject(method = "updateNeighbors(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;II)V", at = @At("TAIL") )
@@ -45,6 +56,4 @@ public abstract class AbstractBlockStateMixin
                     asBlockState(), neighbourPos, pos, flags, maxUpdateDepth);
         }
     }
-
-
 }
---
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixininterfaces/AbstractBlockStateI.java
@@ -0,0 +1,12 @@
+package com.gmail.inayakitorikhurram.fdmc.mixininterfaces;
+
+import com.gmail.inayakitorikhurram.fdmc.Direction4;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+
+public interface AbstractBlockStateI {
+
+    BlockState getStateForNeighborUpdate(Direction4 direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos);
+
+}
