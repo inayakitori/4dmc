f0fdcb0fd8268569585b16f64682dc3cc5a92e99
Inaya Khurram <s3943498@student.rmit.edu.au>
2023-02-09 01:46:33 +1100

    carpet ServerWorldMixin conflict fix Signed-off-by: Inaya Khurram <s3943498@student.rmit.edu.au>
    (cherry picked from commit e4bfa6d42e3b1fee2980b1c43d9386b9f08b75ff)

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/ServerWorldMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/ServerWorldMixin.java
@@ -1,15 +1,12 @@
 package com.gmail.inayakitorikhurram.fdmc.mixin;
 
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.StructureWorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(ServerWorld.class)
-public abstract class ServerWorldMixin
-        extends WorldMixin
-        implements StructureWorldAccess {
+@Mixin(value = ServerWorld.class, priority = 1005)
+public class ServerWorldMixin {
 
     @ModifyConstant(method = "setSpawnPos", constant = @Constant(intValue = 11))
     private int injectedStartRegionRange(int value) {
