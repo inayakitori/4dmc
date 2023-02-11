7e1041635194de2e33755265d3b0822f426a86bd
Mysticpasta1 <ethana5tc@outlook.com>
2023-02-11 13:46:56 -0600

    carpet compat

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/ServerWorldMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/ServerWorldMixin.java
@@ -5,10 +5,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = ServerWorld.class, priority = 1005)
+@Mixin(value = ServerWorld.class, priority = 900)
 public class ServerWorldMixin {
 
-    @ModifyConstant(method = "setSpawnPos", constant = @Constant(intValue = 11))
+    @ModifyConstant(method = "setSpawnPos", constant = @Constant(intValue = 11), require = 0)
     private int injectedStartRegionRange(int value) {
         return 5;
     }
