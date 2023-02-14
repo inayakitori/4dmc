832f191f8472d6edaa912f480a49810c8b89a9db
Inaya Khurram <s3943498@student.rmit.edu.au>
2023-02-14 19:44:02 +1100

    Slime and honey sticks 4Dly

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/block/PistonBlockMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/block/PistonBlockMixin.java
@@ -1,8 +1,16 @@
 package com.gmail.inayakitorikhurram.fdmc.mixin.block;
 
+import com.gmail.inayakitorikhurram.fdmc.math.Direction4Constants;
 import net.minecraft.block.PistonBlock;
+import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PistonBlock.class)
 public class PistonBlockMixin {
+    @Redirect(method = "shouldExtend", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Direction;values()[Lnet/minecraft/util/math/Direction;"))
+    private Direction[] fdmc$shouldExtendfromAnyDirectionPowered(){
+        return Direction4Constants.VALUES;
+    }
 }
