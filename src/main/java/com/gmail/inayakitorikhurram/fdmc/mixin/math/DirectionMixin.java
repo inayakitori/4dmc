df40a4d93647c2cfad06e9b4d6a88fc892f1b43e
Inaya Khurram <s3943498@student.rmit.edu.au>
2023-02-20 15:23:45 +1100

    fluids flow+update 4Dly. fully functional but no visible flow indication yet

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/math/DirectionMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/math/DirectionMixin.java
@@ -292,12 +292,12 @@ public abstract class DirectionMixin implements Direction4 {
         private static final Direction.Type HORIZONTAL4 = fdmc$addType(
                 "HORIZONTAL4",
                 2,
-                new Direction[]{
+                new Direction[]{//this ordering seems to matter for fluid flow
+                        Direction4Constants.KATA,
                         Direction.NORTH,
                         Direction.EAST,
                         Direction.SOUTH,
                         Direction.WEST,
-                        Direction4Constants.KATA,
                         Direction4Constants.ANA
                 },
                 new Direction.Axis[]{
