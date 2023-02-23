5eb1c3f14760a547ce2d75aba116242cdbc547e6
Inaya Khurram <s3943498@student.rmit.edu.au>
2023-02-23 22:23:12 +1100

    WIP adding new chest facing properties

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/math/Direction4Enum.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/math/Direction4Enum.java
@@ -1,7 +1,8 @@
 package com.gmail.inayakitorikhurram.fdmc.math;
 
 import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.Direction4;
-import net.minecraft.util.math.*;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.Nullable;
 
 
