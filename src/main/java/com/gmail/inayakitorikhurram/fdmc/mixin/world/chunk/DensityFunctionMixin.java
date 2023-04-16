55f9d76262e0689b016c117adc60e120bccfa728
Inaya K <inayakitorikhurram@gmail.com>
2023-04-16 21:15:12 +1000

    modified worldgen start: biomes and aquifiers are constant

---
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/world/chunk/DensityFunctionMixin.java
@@ -0,0 +1,24 @@
+package com.gmail.inayakitorikhurram.fdmc.mixin.world.chunk;
+
+import com.gmail.inayakitorikhurram.fdmc.FDMCConstants;
+import net.minecraft.world.gen.densityfunction.DensityFunction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DensityFunction.class)
+public interface DensityFunctionMixin {
+
+    @Mixin(DensityFunction.UnblendedNoisePos.class)
+    class UnblendedNoisePosMixin{
+        @Inject(method = "blockX", at = @At("RETURN"), cancellable = true)
+        private void modifiedBlockX(CallbackInfoReturnable<Integer> cir){
+            int x = cir.getReturnValue();
+            int w4 = (int)(Math.floor(0.5 + (x + 0d)/ FDMCConstants.STEP_DISTANCE));
+            int x4 = x - w4 * FDMCConstants.STEP_DISTANCE;
+            cir.setReturnValue(x4);
+        }
+    }
+}
