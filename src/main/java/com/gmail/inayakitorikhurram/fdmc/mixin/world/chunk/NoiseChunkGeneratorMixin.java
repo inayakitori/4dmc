28ebf274c3253a7acae3afc725a6419adb9737cb
Inaya K <inayakitorikhurram@gmail.com>
2023-04-16 21:15:12 +1000

    modified worldgen start: biomes and aquifiers are constant

---
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/world/chunk/NoiseChunkGeneratorMixin.java
@@ -0,0 +1,12 @@
+package com.gmail.inayakitorikhurram.fdmc.mixin.world.chunk;
+
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(NoiseChunkGenerator.class)
+public class NoiseChunkGeneratorMixin {
+
+
+}
