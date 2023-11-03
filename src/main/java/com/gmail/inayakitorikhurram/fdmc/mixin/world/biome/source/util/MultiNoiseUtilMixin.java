9208404afb002b91fe9eed9cc1599c7c29bf7720
Inaya Khurram <s3943498@student.rmit.edu.au>
2023-11-03 18:49:20 +1100

    Modified biome generation to extract some constants and methods so they can be put in debug menu

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/world/biome/source/util/MultiNoiseUtilMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/world/biome/source/util/MultiNoiseUtilMixin.java
@@ -2,6 +2,7 @@ package com.gmail.inayakitorikhurram.fdmc.mixin.world.biome.source.util;
 
 
 import com.gmail.inayakitorikhurram.fdmc.FDMCConstants;
+import com.gmail.inayakitorikhurram.fdmc.math.BiomeMath;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
 import org.spongepowered.asm.mixin.Final;
@@ -12,11 +13,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Mixin(MultiNoiseUtil.class)
 public class MultiNoiseUtilMixin {
 
     @Mixin(MultiNoiseUtil.MultiNoiseSampler.class)
-    static class MultiNoiseSamplerMixin{
+    static abstract class MultiNoiseSamplerMixin{
         @Shadow @Final private DensityFunction temperature;
 
         @Shadow @Final private DensityFunction humidity;
@@ -29,24 +33,24 @@ public class MultiNoiseUtilMixin {
 
         @Shadow @Final private DensityFunction weirdness;
 
+        @Shadow public abstract DensityFunction temperature();
+
+        @Shadow public abstract DensityFunction humidity();
+
+        @Shadow public abstract DensityFunction continentalness();
+
+        @Shadow public abstract DensityFunction erosion();
+
+        @Shadow public abstract DensityFunction depth();
+
+        @Shadow public abstract DensityFunction weirdness();
+
         @Inject(method = "sample", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Lnet/minecraft/world/gen/densityfunction/DensityFunction$UnblendedNoisePos;<init>(III)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
         private void createNoiseValuePoint(int biomex, int biomey, int biomez, CallbackInfoReturnable<MultiNoiseUtil.NoiseValuePoint> cir, int x3, int y, int z){
 
-
-            int w4 = (int)(Math.floor(0.5 + (x3 + 0d)/ FDMCConstants.STEP_DISTANCE));
-            int x4 = x3 - w4 * FDMCConstants.STEP_DISTANCE;
-
-            DensityFunction.UnblendedNoisePos noisePos3d = new DensityFunction.UnblendedNoisePos(2*x4, y, 2*z);
-            DensityFunction.UnblendedNoisePos noisePos4d = new DensityFunction.UnblendedNoisePos(0, y, w4 * FDMCConstants.BIOMESCALEW);
-
-            cir.setReturnValue(MultiNoiseUtil.createNoiseValuePoint(
-                    (float)(0.63 * this.temperature.sample(noisePos3d)      + 0.77 * this.temperature.sample(noisePos4d)     ),
-                    (float)(0.63 * this.humidity.sample(noisePos3d)         + 0.77 * this.humidity.sample(noisePos4d)        ),
-                    (float)(0.63 * this.continentalness.sample(noisePos3d)  + 0.77 * this.continentalness.sample(noisePos4d) ),
-                    (float)(0.63 * this.erosion.sample(noisePos3d)          + 0.77 * this.erosion.sample(noisePos4d)         ),
-                    (float)(0.63 * this.depth.sample(noisePos3d)            + 0.77 * this.depth.sample(noisePos4d)           ),
-                    (float)(0.63 * this.weirdness.sample(noisePos3d)        + 0.77 * this.weirdness.sample(noisePos4d)       )
-            ));
+            cir.setReturnValue(
+                    BiomeMath.sampleMultiNoise(x3, y, z, (MultiNoiseUtil.MultiNoiseSampler) (Object) this, (MultiNoiseUtil.MultiNoiseSampler) (Object) this)
+            );
         }
     }
 
