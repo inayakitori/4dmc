fb0e9812aa1a2697d6144bbe5900c54027c87009
Inaya Khurram <s3943498@student.rmit.edu.au>
2024-01-18 19:57:58 +0500

    default keybind change. placew+wdirection places block in slice in wdirection. Holding shift places block in current position facing wdirection

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/item/ItemPlacementContextMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/item/ItemPlacementContextMixin.java
@@ -1,15 +1,29 @@
 package com.gmail.inayakitorikhurram.fdmc.mixin.item;
 
 import com.gmail.inayakitorikhurram.fdmc.item.ItemPlacementContext4;
+import com.gmail.inayakitorikhurram.fdmc.math.BlockPos4;
+import com.gmail.inayakitorikhurram.fdmc.math.Direction4Constants;
+import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.CanStep;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
 
 @Mixin(ItemPlacementContext.class)
 public abstract class ItemPlacementContextMixin {
