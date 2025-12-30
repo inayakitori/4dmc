224eeb8edb0799ae8d7ae27acf381a1a9482ab68
iluha168 <illya.chernyshov888@gmail.com>
2025-12-30 19:31:54 +0700

    accurate nether portal destination

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/world/dimension/NetherPortalMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/world/dimension/NetherPortalMixin.java
@@ -1,14 +1,8 @@
 package com.gmail.inayakitorikhurram.fdmc.mixin.world.dimension;
 
-import com.gmail.inayakitorikhurram.fdmc.math.BlockPos4;
 import com.gmail.inayakitorikhurram.fdmc.math.Direction4Constants;
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.dimension.NetherPortal;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
@@ -17,8 +11,6 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Optional;
 
 @Mixin(NetherPortal.class)
 public abstract class NetherPortalMixin {
@@ -37,17 +29,6 @@ public abstract class NetherPortalMixin {
 //            cir.cancel();
 //        }
 //    }
-
-    @WrapMethod(method = "getNewPortal")
-    private static Optional<NetherPortal> fdmc$onlyAllowPortalOnSpecificSlice(WorldAccess world, BlockPos pos, Direction.Axis firstCheckedAxis, Operation<Optional<NetherPortal>> static$getNewPortal){
-        int w = BlockPos4.of(pos).getW4();
-        if(w % 8 == 0 || world.getDimension().coordinateScale() == 8.0){
-            // it lies
-            return static$getNewPortal.call(world, pos, firstCheckedAxis);
-        } else {
-            return Optional.empty();
-        }
-    }
 
     @Redirect(method = "getOnAxis", at = @At(value = "FIELD", target = "Lnet/minecraft/util/math/Direction;SOUTH:Lnet/minecraft/util/math/Direction;", opcode = Opcodes.GETSTATIC))
     private static Direction negativeDirUseW(@Local(argsOnly = true) Direction.Axis axis) {
