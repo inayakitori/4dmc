80c3de587faa8024c2b7455402ba4f68ee551b94
iluha168 <illya.chernyshov888@gmail.com>
2026-01-17 17:01:26 +0700

    fix: `ClientPlayerEntity` distanceMoved

--- src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/client/network/ClientPlayerEntityMixin.java
+++ src/main/java/com/gmail/inayakitorikhurram/fdmc/mixin/client/network/ClientPlayerEntityMixin.java
@@ -1,14 +1,18 @@
 package com.gmail.inayakitorikhurram.fdmc.mixin.client.network;
 
 import com.gmail.inayakitorikhurram.fdmc.math.Vec3f;
+import com.gmail.inayakitorikhurram.fdmc.math.Vec4d;
 import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.CanPlaceW;
 import com.gmail.inayakitorikhurram.fdmc.mixininterfaces.SidewaysSpeedW;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalDoubleRef;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -41,5 +45,22 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
     )
     void fdmc$setSidewaysSpeedW(CallbackInfo ci, @Local Vec2f movementSpeed){
         setSidewaysSpeedW(Vec3f.of(movementSpeed).z);
+    }
+
+    @Redirect(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getX()D", ordinal = 0))
+    double move$before$measureX4(ClientPlayerEntity entity, @Share("posW") LocalDoubleRef beforeW){
+        Vec4d pos = Vec4d.of(entity.getEntityPos());
+        beforeW.set(pos.w);
+        return pos.x4;
+    }
+    @Redirect(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;getX()D", ordinal = 1))
+    double move$after$measureX4(ClientPlayerEntity entity, @Share("posW") LocalDoubleRef beforeW){
+        Vec4d pos = Vec4d.of(entity.getEntityPos());
+        beforeW.set(pos.w - beforeW.get()); // Now contains dw
+        return pos.x4;
+    }
+    @Redirect(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;hypot(FF)F"))
+    float move$after$addDistanceMoved4(float dx, float dz, @Share("posW") LocalDoubleRef dw){
+        return (float) new Vec3d(dx, dz, dw.get()).length();
     }
 }
